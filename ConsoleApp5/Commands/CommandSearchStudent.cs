1078258f52bdd0b35bd53a82f74455b804d426e7
Данил Тимофеев <danil2006timofeev@gmail.com>
2023-11-14 08:55:35 +0300

    Search работает

--- ConsoleApp5/Commands/CommandSearchStudent.cs
+++ ConsoleApp5/Commands/CommandSearchStudent.cs
@@ -9,9 +9,9 @@
 
     public override void Execute()
     {
-        Console.Write("Введите имя студента: ");
-        List<Student> students = new List<Student>();
-        string studentData = Console.ReadLine();
-        Console.WriteLine(DB.StudentDB.Search(studentData));
+        Console.Write("Введите имя или фамилию студента: ");
+        List<Student> students = studentDB.Search(Console.ReadLine());
+        for (int i = 0; i < students.Count; i++)
+        { Console.WriteLine($"{i + 1}. {students[i].FirstName} {students[i].LastName} UID: {students[i].UID}"); }
     }
 }
