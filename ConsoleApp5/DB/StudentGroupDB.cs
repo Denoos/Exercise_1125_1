368f495e190736ccb582c9408a595d7c6dcdf5ac
Данил Тимофеев <danil2006timofeev@gmail.com>
2023-11-16 09:40:46 +0300

    f

---
+++ ConsoleApp5/DB/StudentGroupDB.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+internal class StudentGroupDB
+{
+    public string group { get; set; }
+    public string student { get; set; }
+    internal StudentGroupDB() {  }
+    Dictionary<string, Group> groups = JsonSerializer.Deserialize<Dictionary<string, Group>>(File.ReadAllText("groups.json"));
+    Dictionary<string, Student> students = JsonSerializer.Deserialize<Dictionary<string, Student>>(File.ReadAllText("students.json"));
+
+}
